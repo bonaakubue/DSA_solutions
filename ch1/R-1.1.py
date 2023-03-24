3a676244efcb352de07cbfe90181e7e3b4e58f15
Bona Akubue <bonaakubue@gmail.com>
2023-03-24 20:48:40 +0100

    first commit

---
+++ ch1/R-1.1.py
@@ -0,0 +1,14 @@
+'''
+Write a short Python function, is multiple(n, m), 
+that takes two integer values and returns True if n is a
+ multiple of m, that is, n = mi for some integer i, and False otherwise.
+ '''
+
+def multiple(n, m):
+    if n%m == 0:
+        return True
+    return False
+
+#Testing
+result = multiple(4,2)
+print(result)
